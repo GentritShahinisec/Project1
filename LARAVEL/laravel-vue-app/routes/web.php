41ca32a45845f76b056ea7ab468332ec7d533859
GentritShahinisec <g.shahini@sec-storage.co.uk>
2025-04-13 13:52:33 +0200

    Update web.php

--- LARAVEL/laravel-vue-app/routes/web.php
+++ LARAVEL/laravel-vue-app/routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\Api\ExpensController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// qetu i krijon routat
+
+Route::get('/dashboard', ExpensController::class, 'index')->name('dashboard');
